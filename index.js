db6fce65d6baddfd2621f859dcee3f2c4e2d95da
Kris Henderson <krishenderson@gmail.com>
2020-01-28 15:11:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-// Code your solution here!
+// function printString(myString) {
+//   console.log(myString[0]);
+  
+//   if(myString.length >1){
+//     let mySubString = myString.substring(1, myString.length);
+//     printString(mySubString);
+//   } else {
+//     return true;
+//   }
+// }
 
+function reverseString(myString) {
+  if(myString.length > 1) {
+    
+  }
+}
+
+function addUpTo(array, index) {
+if (index === 0) {
+    return array[index];
+  } else {
+    return array[index] + addUpTo(array, index - 1)
+  }
+}
